4f382163c3b187ac175add0743463f546a3b1f53
Lais Lemos <laisaplemos10@gmail.com>
2024-10-14 19:14:30 -0300

    logica da tela de redefinir senha

--- app/src/main/java/br/edu/puccampinas/starfocusapp/ResetPassword.kt
+++ app/src/main/java/br/edu/puccampinas/starfocusapp/ResetPassword.kt
@@ -1,6 +1,51 @@
 package br.edu.puccampinas.starfocusapp
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import br.edu.puccampinas.starfocusapp.databinding.ResetPasswordBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ResetPassword : AppCompatActivity() {
+
+    // ViewBinding
+    private val binding by lazy { ResetPasswordBinding.inflate(layoutInflater) }
+    // Configuração do FirebaseAuth
+    private val auth by lazy { FirebaseAuth.getInstance() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        with(binding) {
+
+            btnSend.setOnClickListener {
+                forgotPassword(idEmail.text.toString())
+            }
+
+            signUp.setOnClickListener {
+                finish()
+            }
+        }
+    }
+
+    private fun forgotPassword(email: String) {
+        if (email.isBlank()) {   // Verifica se o campo de e-mail está em branco
+            Toast.makeText(this, "Digite o endereço de email", Toast.LENGTH_LONG).show()
+            return
+        }
+        // Envia um e-mail de redefinição de senha para o endereço fornecido
+        auth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // Se a operação for bem-sucedida, mostra um dialog e volta para a tela de login
+                Toast.makeText(this, "Email enviado", Toast.LENGTH_LONG).show()
+                startActivity(Intent(this@ResetPassword, Login::class.java))
+                finish()
+
+            } else {
+                // Se a operação falhar, exibe uma mensagem de erro
+                Toast.makeText(this, "Endereço de email inválido!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 }
